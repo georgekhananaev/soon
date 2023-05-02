9d76dde28989c6590d5ef320088b2b3f8bd3d15a
George Khananaev <106206490+georgekhananaev@users.noreply.github.com>
2023-05-02 08:12:47 +0300

    reactatomics initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -25,23 +25,23 @@ function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover" />
 
-        <title>Revolicon - Beautifully Crafted Open Source Icons</title>
-        <meta name="title" content="Revolicon - Beautifully Crafted Open Source Icons" />
-        <meta name="description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
-        <meta name="keywords" content="Revolicon, Font Icon, Web Icon, Icons, Icon, SVG Icon, SVG, Font, Revolution" />
+        <title>ReactAtomics - Beautifully Crafted Open Source Icons</title>
+        <meta name="title" content="ReactAtomics - Beautifully Crafted Open Source Icons" />
+        <meta name="description" content="ReactAtomics is an AI-based architecture that self-designs and constructs websites." />
+        <meta name="keywords" content="ReactAtomics, AI Revolution" />
 
-        <meta property="og:site_name" content="Revolicon" />
+        <meta property="og:site_name" content="ReactAtomics" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://revolicon.com/" />
-        <meta property="og:title" content="Revolicon - Beautifully Crafted Open Source Icons" />
-        <meta property="og:description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
+        <meta property="og:url" content="https://reactatomics.com/" />
+        <meta property="og:title" content="ReactAtomics - Where art meets artificial intelligence" />
+        <meta property="og:description" content="ReactAtomics is an AI-based architecture that self-designs and constructs websites." />
         <meta property="og:image" content="/thumbnail.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://revolicon.com/" />
-        <meta property="twitter:title" content="Revolicon - Beautifully Crafted Open Source Icons" />
-        <meta property="twitter:description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
+        <meta property="twitter:url" content="https://reactatomics.com/" />
+        <meta property="twitter:title" content="ReactAtomics - Beautifully Crafted Open Source Icons" />
+        <meta property="twitter:description" content="ReactAtomics is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
         <meta property="twitter:image" content="/thumbnail.png" />
       </Head>
       <Component {...pageProps} />
